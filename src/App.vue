251c29e92c1fe58673d6a9e138f34bd8c746e1b5
ttmars <2292752593@qq.com>
2022-11-15 15:46:46 +0800

    音乐播放器

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,75 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <el-container>
+
+    <el-header>
+      <div class="mt-4">
+        <el-input v-model="input" placeholder="Please input" class="input-with-select">
+          <template #prepend>
+            <el-select v-model="select" placeholder="Select" style="width: 115px">
+              <el-option label="网易云" value="1" />
+              <el-option label="咪咕" value="2" />
+            </el-select>
+          </template>
+          <template #append>
+            <el-button icon="Search" @click="fetchData" />
+          </template>
+        </el-input>
+      </div>
+    </el-header>
+
+    <el-main>
+      <el-scrollbar height="400px">
+
+        <el-row v-for="todo in todoData">
+          <el-col :span="8">
+            {{ todo.Name }}
+          </el-col>
+          <el-col :span="8">
+            {{ todo.Singer }}
+          </el-col>
+          <el-col :span="8">
+            <el-button @click="playMusic(todo)" icon="VideoPlay" plain />
+          </el-col>
+        </el-row>
+      </el-scrollbar>
+    </el-main>
+
+    <el-footer>
+      <audio controls="controls" :src="curMusicSrc" autoplay="autoplay">
+      </audio>
+    </el-footer>
+  </el-container>
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<script>
+export default {
+  data() {
+    return {
+      input: '周杰伦',
+      select: '1',
+      todoData: [{ AlbumName: null, AlbumPic: null, Alia: null, Audio: null, Flac: null, ID: null, Lyric: null, Name: null, Singer: null, Size: null, Time: null }],
+      curMusicSrc: ''
+    }
+  },
+  methods: {
+    async fetchData() {
+      this.todoData = null
+      let url = 'http://www.youthsweet.com:3800/migu/api?keyword=' + this.input
+      const res = await fetch(url)
+      this.todoData = await res.json()
+      this.curMusicSrc = this.todoData[0].Audio
+    },
+    playMusic(todo) {
+      this.curMusicSrc = todo.Audio
+    }
+  },
+  mounted() {
+    this.fetchData()
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+</script>
+
+<style>
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+const app = createApp(App)
 
 // 引入ElementPlus
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-
-const app = createApp(App)
 app.use(ElementPlus)
+
+// 引入图标
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
 app.mount('#app')
